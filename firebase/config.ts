45784b710350afde594640f0f25f73bb6ea720c2
heliosasdesigner <heliogramlaboratory@gmail.com>
2025-04-14 11:07:31 +0100

    final push

--- firebase/config.ts
+++ firebase/config.ts
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-console.log("FIREBASE_API_KEY", FIREBASE_API_KEY);
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
